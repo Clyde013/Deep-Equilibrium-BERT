a6e650ec9efeaf6451258cce108217c4be5cf35d
Clyde <weipyn@outlook.com>
2022-05-19 00:33:17 +0800

    m3a Neural CNN ODE Tutorial updated
    Can serve as reference. There are some shitty workarounds to be done.

--- BertODE/bertode.py
+++ BertODE/bertode.py
@@ -7,8 +7,8 @@ from packaging import version
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
-from ...activations import ACT2FN, gelu
-from ...modeling_outputs import (
+from transformers.activations import ACT2FN, gelu
+from transformers.modeling_outputs import (
     BaseModelOutputWithPastAndCrossAttentions,
     BaseModelOutputWithPoolingAndCrossAttentions,
     CausalLMOutputWithCrossAttentions,
@@ -18,16 +18,16 @@ from ...modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
 )
-from ...modeling_utils import PreTrainedModel
-from ...pytorch_utils import apply_chunking_to_forward, find_pruneable_heads_and_indices, prune_linear_layer
-from ...utils import (
+from transformers.modeling_utils import PreTrainedModel
+from transformers.pytorch_utils import apply_chunking_to_forward, find_pruneable_heads_and_indices, prune_linear_layer
+from transformers.utils import (
     add_code_sample_docstrings,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
     logging,
     replace_return_docstrings,
 )
-from .configuration_roberta import RobertaConfig
+from transformers.models.configuration_roberta import RobertaConfig
 
 
 logger = logging.get_logger(__name__)
