f9f681a3ec387ddd566c187ecd1ca6ee89f20d44
Clyde013 <weipyn@outlook.com>
2022-11-20 15:43:19 +0800

    update train script

--- test_scripts/test_mlm.py
+++ test_scripts/test_mlm.py
@@ -5,11 +5,14 @@ from DEQBert.modeling_deqbert import DEQBertForMaskedLM
 from DEQBert.configuration_deqbert import DEQBertConfig
 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 config = DEQBertConfig.from_pretrained("../DEQBert/model_card/config.json")
 model = DEQBertForMaskedLM(config)
+model = model.to(device)
 
 tokenizer = DEQBertTokenizer.from_pretrained("../DEQBert/model_card/")
 inputs = tokenizer("The capital of France is <mask>.", return_tensors="pt")
+inputs = inputs.to(device)
 
 print(inputs)
 print(model(**inputs))
--- train.py
+++ train.py
@@ -1,19 +1,22 @@
 import pytorch_lightning as pl
 from TrainDatasets import oscar
 
-from transformers import RobertaTokenizer, DataCollatorForLanguageModeling
+from transformers import DataCollatorForLanguageModeling
+from DEQBert.tokenization_deqbert import DEQBertTokenizer
 from DEQBert.configuration_deqbert import DEQBertConfig
 from transformers import Trainer, TrainingArguments
 from DEQBert.modeling_deqbert import DEQBertForMaskedLM
 
 import wandb
+import torch
 
+# To specify the GPU to use you have to set the CUDA_VISIBLE_DEVICES="0" environment variable
 wandb.init(project="DEQBert",
            name="test-run")
 
-config = DEQBertConfig.from_pretrained("roberta-base")
+config = DEQBertConfig.from_pretrained("DEQBert/model_card/config.json")
 config.is_decoder = False
-tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+tokenizer = DEQBertTokenizer.from_pretrained("roberta-base")
 
 model = DEQBertForMaskedLM(config=config)
 
@@ -34,8 +37,9 @@ training_args = TrainingArguments(
     save_total_limit=2,
     prediction_loss_only=True,
     logging_steps=100,
-#    report_to="wandb"
+    report_to="wandb"
 )
+
 
 trainer = Trainer(
     model=model,
