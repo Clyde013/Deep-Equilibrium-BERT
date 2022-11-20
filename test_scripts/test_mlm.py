6cfa8b84f3ff658cc593a1a02d6441c909c445da
Clyde013 <weipyn@outlook.com>
2022-11-20 15:23:45 +0800

    implement DEQBertTokenizer
    i love copy pasting roberta source code

--- test_scripts/test_mlm.py
+++ test_scripts/test_mlm.py
@@ -1,14 +1,15 @@
 import torch
 from transformers import RobertaTokenizer
-
-from DEQBert.deqbert import DEQBertForMaskedLM
+from DEQBert.tokenization_deqbert import DEQBertTokenizer
+from DEQBert.modeling_deqbert import DEQBertForMaskedLM
 from DEQBert.configuration_deqbert import DEQBertConfig
 
 
 config = DEQBertConfig.from_pretrained("../DEQBert/model_card/config.json")
 model = DEQBertForMaskedLM(config)
 
-tokenizer = RobertaTokenizer.from_pretrained("../DEQBert/model_card/")
+tokenizer = DEQBertTokenizer.from_pretrained("../DEQBert/model_card/")
 inputs = tokenizer("The capital of France is <mask>.", return_tensors="pt")
 
+print(inputs)
 print(model(**inputs))
