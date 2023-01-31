2a1e57da47df8cef316d18d5f9cb6cd5f1014c92
Clyde013 <weipyn@outlook.com>
2023-01-31 09:48:20 +0800

    Fix train_profiler.py directory errors
    Fixes the filepaths and import errors.
    Running the file from the root directory has to be done as a module,
    allowing relative directories and imports to work properly.
    Additionally, updated the README to reflect this change.

--- profiler/README.md
+++ profiler/README.md
@@ -1,5 +1,8 @@
 Running train_profiler.py should be done only from the root directory,
 otherwise the relative file paths and wandb config file will not work.
+```python
+python -m profiler.train_profiler
+```
 
 The script will run one step of the training loop under the profiler's
 context manager. The outputs will be the prof.key_average() as a pickle
--- profiler/train_profiler.py
+++ profiler/train_profiler.py
@@ -1,6 +1,6 @@
 import pickle
 
-from TrainDatasets import the_pile
+import datasets
 from transformers import DataCollatorForLanguageModeling 
 from DEQBert.tokenization_deqbert import DEQBertTokenizer
 from DEQBert.configuration_deqbert import DEQBertConfig
@@ -13,75 +13,83 @@ import wandb
 from torch.profiler import profile, record_function, ProfilerActivity
 from torch.optim.lr_scheduler import OneCycleLR
 
-# To specify the GPU to use you have to set the CUDA_VISIBLE_DEVICES="0" environment variable
-wandb.init(project="DEQBert-Profiler")
-wandb.run.name = wandb.config.run_name
-wandb.run.save()
+if __name__ == "__main__":
+    # To specify the GPU to use you have to set the CUDA_VISIBLE_DEVICES="0" environment variable
+    wandb.init(project="DEQBert-Profiler")
+    wandb.run.name = wandb.config.run_name
+    wandb.run.save()
 
-config = DEQBertConfig.from_pretrained("../DEQBert/model_card/config.json")
-config.is_decoder = False
-config.hidden_dropout_prob = wandb.config.hidden_dropout
-config.attention_probs_dropout_prob = wandb.config.attention_dropout
+    config = DEQBertConfig.from_pretrained("DEQBert/model_card/config.json")
+    config.is_decoder = False
+    config.hidden_dropout_prob = wandb.config.hidden_dropout
+    config.attention_probs_dropout_prob = wandb.config.attention_dropout
 
-tokenizer = DEQBertTokenizer.from_pretrained("roberta-base")
+    tokenizer = DEQBertTokenizer.from_pretrained("roberta-base")
 
-with profile(activities=[
-        ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
-    with record_function("model_inference"):
+    with profile(activities=[
+            ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+        with record_function("model_inference"):
 
-        model = DEQBertForMaskedLM(config=config)
-        if wandb.config.load_checkpoint is not None:
-            # we load checkpoints from state_dicts directly instead of using trainer.save(resume_from_checkpoint=...)
-            # because this allows us to alter scheduler hyperparameters when resuming training.
-            model = model.from_pretrained(wandb.config.load_checkpoint)
+            model = DEQBertForMaskedLM(config=config)
+            if wandb.config.load_checkpoint is not None:
+                # we load checkpoints from state_dicts directly instead of using trainer.save(resume_from_checkpoint=...)
+                # because this allows us to alter scheduler hyperparameters when resuming training.
+                model = model.from_pretrained(wandb.config.load_checkpoint)
 
-        pile_datamodule = the_pile.PileDataModule(tokenizer, max_seq_length=tokenizer.model_max_length)
-        pile_datamodule.setup()
-        pile_dataset = pile_datamodule.dataset
+            # while would preferably use the_pile datamodule, it is inconvenient to download the entire dataset + map the
+            # encoding function to it. So we simply copy-paste the relevant parts and apply it to the hacker_news subset
+            def encode(example_batch):
+                # tokenize the text
+                features = tokenizer(example_batch["text"], max_length=128, padding="max_length",
+                                          truncation=True, return_tensors="pt")
+                return features
+            pile_dataset = datasets.load_dataset("the_pile", streaming=False, split="train", subsets=["hacker_news"]).select(range(32))
+            pile_dataset = pile_dataset.shuffle(seed=69)
+            pile_dataset = pile_dataset.map(encode, batched=True, remove_columns=["text", "meta"]).with_format("torch")
 
-        data_collator = DataCollatorForLanguageModeling(
-            tokenizer=tokenizer, mlm=True, mlm_probability=wandb.config.mlm_probability
-        )
+            data_collator = DataCollatorForLanguageModeling(
+                tokenizer=tokenizer, mlm=True, mlm_probability=wandb.config.mlm_probability
+            )
 
-        # onecycle learning rate scheduler. Thanks lucas.
-        optimizer = AdamW(model.parameters(),
-                          betas=(wandb.config.adam_beta1, wandb.config.adam_beta2),
-                          eps=wandb.config.adam_epsilon,
-                          weight_decay=wandb.config.weight_decay)
-        scheduler = OneCycleLR(optimizer, max_lr=wandb.config.learning_rate, total_steps=int(wandb.config.total_steps),
-                               last_epoch=-1)
-        # the scheduler has to initialise initial_lr for the optimizer, before we can set last_epoch. After the scheduler
-        # steps once the learning rate will be set to as if it was at last_epoch.
-        scheduler.last_epoch = wandb.config.resume_steps
+            # onecycle learning rate scheduler. Thanks lucas.
+            optimizer = AdamW(model.parameters(),
+                              betas=(wandb.config.adam_beta1, wandb.config.adam_beta2),
+                              eps=wandb.config.adam_epsilon,
+                              weight_decay=wandb.config.weight_decay)
+            scheduler = OneCycleLR(optimizer, max_lr=wandb.config.learning_rate, total_steps=int(wandb.config.total_steps),
+                                   last_epoch=-1)
+            # the scheduler has to initialise initial_lr for the optimizer, before we can set last_epoch. After the scheduler
+            # steps once the learning rate will be set to as if it was at last_epoch.
+            scheduler.last_epoch = wandb.config.resume_steps
 
-        training_args = TrainingArguments(
-            output_dir='../models/profiler/',
-            max_steps=1,
-            per_device_train_batch_size=wandb.config.batch_size,
-            gradient_accumulation_steps=wandb.config.grad_accum_steps,
-            prediction_loss_only=True,
-            logging_steps=wandb.config.logging_steps,
-            report_to="wandb"
-        )
+            training_args = TrainingArguments(
+                output_dir='../models/profiler/',
+                max_steps=1,
+                per_device_train_batch_size=wandb.config.batch_size,
+                gradient_accumulation_steps=wandb.config.grad_accum_steps,
+                prediction_loss_only=True,
+                logging_steps=wandb.config.logging_steps,
+                report_to="wandb"
+            )
 
 
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            data_collator=data_collator,
-            train_dataset=pile_dataset,
-            optimizers=(optimizer, scheduler)
-        )
+            trainer = Trainer(
+                model=model,
+                args=training_args,
+                data_collator=data_collator,
+                train_dataset=pile_dataset,
+                optimizers=(optimizer, scheduler)
+            )
 
-        print("profiling training...")
-        trainer.train()
+            print("profiling training...")
+            trainer.train()
 
-print("profiling complete, exporting trace...")
-# view this file with chrome://tracing
-prof.export_chrome_trace("results/trace.json")
+    print("profiling complete, exporting trace...")
+    # view this file with chrome://tracing
+    prof.export_chrome_trace("profiler/results/trace.json")
 
-print("pickling profiler...")
-# just in case, we save the profiler object for later analysis
-outfile = open('results/profiler.pickle', 'wb')
-pickle.dump(prof.key_averages(), outfile)
-outfile.close()
+    print("pickling profiler...")
+    # just in case, we save the profiler object for later analysis
+    outfile = open('profiler/results/profiler.pickle', 'wb')
+    pickle.dump(prof.key_averages(), outfile)
+    outfile.close()
