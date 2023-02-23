12d14ec7d4f8fc39d942f40d861fedce31716b8d
Clyde013 <weipyn@outlook.com>
2023-02-23 10:16:16 +0800

    Add local streaming of the_pile dataset
    I just realised no one can realistically load the entire pile dataset
    into memory for huggingface to tokenize it. So we download and stream
    the cached datafiles instead, and the mapping function will be applied
    as the data is loaded.

--- TrainDatasets/the_pile.py
+++ TrainDatasets/the_pile.py
@@ -8,7 +8,10 @@ _HOST_URL = "https://the-eye.eu"
 _TRAIN_SOURCE_FILES = {"train": [f"{_HOST_URL}/public/AI/pile/train/{i:0>2}.jsonl.zst" for i in range(30)]}
 _USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
               "Chrome/86.0.4240.75 Safari/537.36"
-
+# either set this variable or environment variable in the cmd with export HF_DATASETS_CACHE="/path/to/another/directory"
+_CACHE_DIR = "/mnt/raid1/data"
+# this should be the directory where the pile datafiles are extracted once the download is complete, may be different.
+_EXTRACTED_FILES = {"train": f"{_CACHE_DIR}/downloads/extracted/*"}
 
 class PileDataModule(LightningDataModule):
     """
@@ -21,7 +24,7 @@ class PileDataModule(LightningDataModule):
             self,
             tokenizer: PreTrainedTokenizerBase,
             stream: bool = False,
-            buffer_size: int = 10000,
+            buffer_size: int = 10_000,
             max_seq_length: int = 128,
             train_batch_size: int = 32,
             eval_batch_size: int = 32,
@@ -40,14 +43,19 @@ class PileDataModule(LightningDataModule):
     def setup(self, stage: str = None):
         if self.stream:
             self.dataset = datasets.load_dataset("the_pile", streaming=True, split="train", subsets=["all"])
-            self.dataset = self.dataset.shuffle(seed=69, buffer_size=self.buffer_size)
         else:
+            # first download the dataset from online
             config = DownloadConfig(resume_download=True,
                                     user_agent=_USER_AGENT,
-                                    max_retries=10)
-            self.dataset = datasets.load_dataset("json", data_files=_TRAIN_SOURCE_FILES, download_config=config)
-            self.dataset = self.dataset.shuffle(seed=69)
+                                    max_retries=100)
+            # if the dataset is already downloaded, it will resolve the files as cached
+            self.dataset = datasets.load_dataset("json", data_files=_TRAIN_SOURCE_FILES, download_config=config,
+                                                 cache_dir=_CACHE_DIR)
+            # then we stream the local dataset, since it's actually impossible to load the entire pile dataset into
+            # memory and map the tokenizer function over it
+            self.dataset = datasets.load_dataset("json", data_files=_EXTRACTED_FILES, split="train", streaming=True)
 
+        self.dataset = self.dataset.shuffle(seed=69, buffer_size=self.buffer_size)
         # tokenize the dataset. scuffed af to manually remove denote the remove_columns but it works
         self.dataset = self.dataset.map(self.encode, batched=True, remove_columns=["text", "meta"]).with_format("torch")
 
